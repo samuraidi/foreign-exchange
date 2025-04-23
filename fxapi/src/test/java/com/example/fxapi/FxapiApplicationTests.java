4d9275034f1f13a31da6b6f287412c34659a47c7
samuraidi <33642164+samuraidi@users.noreply.github.com>
2025-04-23 12:34:36 +0300

    Add files via upload

---
+++ fxapi/src/test/java/com/example/fxapi/FxapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
